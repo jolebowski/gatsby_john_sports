2c716d1a92d9aa5f2458cf092534fc0cf4df9b3d
jolebowski <jolebowski@gmail.com>
2020-03-04 11:24:35 +0100

    edit navbar file and add css

--- src/components/Globals/Navbar.js
+++ src/components/Globals/Navbar.js
@@ -1,64 +1,73 @@
 import React, { Component } from "react"
-import { Link } from 'gatsby'
-import { FaCartArrowDown } from 'react-icons/fa'
-import logo  from '../../images/iconfinder.png'
+import { Link } from "gatsby"
+import { FaCartArrowDown } from "react-icons/fa"
+import logo from "../../images/iconfinder.png"
 
 export default class Navbar extends Component {
-    state = {
-        navBarOpen: false,
-        css: 'collapse navbar-collapse',
-        links: [{
-            id: 1,
-            path: '/',
-            text: 'home'
-        }, {
-            id: 2,
-            path: '/about',
-            text: 'about'
-        }]
-    }
+  state = {
+    navBarOpen: false,
+    css: "collapse navbar-collapse",
+    links: [
+      {
+        id: 1,
+        path: "/",
+        text: "Accueil",
+      },
+      {
+        id: 2,
+        path: "/about",
+        text: "A propos",
+      },
+    ],
+  }
 
-    navBarHandler = () => {
-       this.state.navBarOpen ?
-       this.setState({css: 'collapse navbar-collapse', navBarOpen: false}) 
-       : this.setState({css: 'collapse navbar-collapse show', navBarOpen: true})
-    }
+  navBarHandler = () => {
+    this.state.navBarOpen
+      ? this.setState({ css: "collapse navbar-collapse", navBarOpen: false })
+      : this.setState({
+          css: "collapse navbar-collapse show",
+          navBarOpen: true,
+        })
+  }
 
-    render() {
-         
-        return (
-            <nav className="navbar
+  render() {
+    return (
+      <nav
+        className="navbar
              navbar-expand-sm bg-light 
-             navbar-light">
-                <Link to="/" className="navbar-brand">
-                    <img src={logo} alt="logo" />
-                </Link>
-                <button  
-                    className="navbar-toggler" type='button'
-                    onClick={this.navBarHandler}
-                    >
-                    <span className="navbar-toggler-icon" />
-                </button>
-                <div className={this.state.css}>
-                    <ul className="navbar-nav mx-auto">
-                        {
-                            this.state.links.map(link => {
-                                return (
-                                    <li key={link.id} className='nav-item'>
-                                        <Link to={link.path} className='nav-link
-                                        text-capitalize'>
-                                            {link.text}
-                                        </Link>
-                                    </li>
-                                )
-                            })
-                        }
-                        <li className='nav-item ml-sm-5'>
-                            <FaCartArrowDown className="car-icon" />
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        )
-    }
+             navbar-light"
+      >
+        <Link to="/" className="navbar-brand">
+          <img src={logo} alt="logo" />
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          onClick={this.navBarHandler}
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className={this.state.css}>
+          <ul className="navbar-nav mx-auto">
+            {this.state.links.map(link => {
+              return (
+                <li key={link.id} className="nav-item">
+                  <Link
+                    to={link.path}
+                    className="nav-link
+                    text-capitalize"
+                  >
+                    {link.text}
+                  </Link>
+                </li>
+              )
+            })}
+            <li className="nav-item ml-sm-5">
+              <FaCartArrowDown className="car-icon" />
+            </li>
+          </ul>
+        </div>
+      </nav>
+    )
+  }
 }

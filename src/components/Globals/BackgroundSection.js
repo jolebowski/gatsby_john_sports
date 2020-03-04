3744f0cfb38e82776f34e0df9f81c754b0e1bbfb
jolebowski <jolebowski@gmail.com>
2020-03-04 14:42:14 +0100

    update backgroundSection file

--- src/components/Globals/BackgroundSection.js
+++ src/components/Globals/BackgroundSection.js
@@ -2,7 +2,7 @@ import React from "react"
 import BackgroundImage from "gatsby-background-image"
 
 const BackgroundSection = ({ img, styleClass, title }) => (
-  <BackgroundImage className={styleClass} fluid={img} durationFadeIn={5000}>
+  <BackgroundImage className={styleClass} fluid={img}>
     <h1 className="title text-white text-uppercase text-center display-4 font-weight-bold">
       {title}
     </h1>

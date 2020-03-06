9a9f7699c7016f9537166f9e3ce467a05c755bf2
jolebowski <jolebowski@gmail.com>
2020-03-06 12:36:11 +0100

    edit for test

--- src/components/Home/Product.js
+++ src/components/Home/Product.js
@@ -2,12 +2,11 @@ import React from "react"
 import Img from "gatsby-image"
 const Product = ({ product }) => (
   <div className="col-10 col-sm-8 col-md-6 col-lg-4 mx-auto my-3">
-    <div className="card" style={{ mineHeight: "100%" }}>
+    <div className="card">
       <Img fluid={product.image.fluid} className="card-img-top" />
       <div className="card-body text-center">
         <h6>{product.title}</h6>
         <h6>{product.price} €</h6>
-        {console.log(product, "productttt")}
         <button
           className="btn btn-orange mt-3 text-capitalize snipcart-add-item"
           data-item-id={product.id}
